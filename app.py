67a567b8326e919b45cbfdbde4314bea3ac61921
jojin77 <99331146+jojin77@users.noreply.github.com>
2023-07-07 10:09:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.INFO)
 logging.getLogger().addHandler(console_handler)
 
-# Get the environment variables
+# Get the environment variables and run-check all ENV's
 app_token = os.environ.get("APP_TOKEN")
 bot_token = os.environ.get("BOT_TOKEN")
 target_channel_id = os.environ.get("TARGET_CHANNEL_ID")
@@ -29,8 +29,6 @@ if not target_channel_id:
     logging.warning('TARGET_CHANNEL_ID not found in env.')
 if not channel_ids:
     logging.warning('CHANNEL_IDS not found in env.')
-#if not python_encoding:
-#    logging.warning('PYTHONIOENCODING not found in env.')
 
 if not all([app_token, bot_token, target_channel_id, channel_ids]):
     logging.warning('Missing required environment variables. Aborting...')
@@ -47,11 +45,6 @@ def load_filter_patterns():
         logging.error("Failed to load filter patterns")
         logging.error(traceback.format_exc())
         sys.exit(1)
-#def load_filter_patterns():
-#    patterns_file = "/appz/scripts/patterns.json"
-#    with open(patterns_filee, 'r') as file:
-#        data = json.load(file)
-#        return data.get('patterns', [])
 
 # Listens to incoming messages from the specified channels and filters based on patterns
 @app.message(re.compile("|".join(load_filter_patterns())))
